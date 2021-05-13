7497986bf4ba85d8f6c007ea767323f5b34c97f3
Aad Versteden <madnificent@gmail.com>
2021-05-13 10:44:05 +0200

    Front-facing call to send out an email with a new key
    It is now possible to send a call to this service, which will then send
    you an email with a new key if your address exists.

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 // see https://github.com/mu-semtech/mu-javascript-template for more info
+import { app, errorHandler } from 'mu';
+import generateKey from './util/generate-key';
+import { insertKeyForAccount, getAccountForEmail, AccountNotFoundError, syncAccountFromInter } from './queries';
+import { sendLoginEmail } from './emails';
 
-import { app, query, errorHandler } from 'mu';
+app.post('/emails/:email/keys', async function(req, res) {
+  try {
+    // 1. generate a new key
+    const key = generateKey(32);
+    const email = req.params.email;
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
+    // 2. update the account graph
+    await syncAccountFromInter( email );
+
+    // 3. get the account link
+    const account = await getAccountForEmail( email );
+
+    // 3. add data if account exists for key
+    await insertKeyForAccount( key, account );
+
+    // 4. send out the email
+    await sendLoginEmail( email, key );
+
+    // 5. respond positive
+    res.status(200).send(JSON.stringify({message: "Email sent"}));
+  } catch (e) {
+    if( e instanceof AccountNotFoundError ) {
+      // maybe throw error
+      res.status(500).send(JSON.stringify({message: "Something went wrong"}));
+    }
+  }
+});
 
 app.use(errorHandler);
